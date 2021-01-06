c609e16bcf2b2e239b9ea1ba85bbe9582926562e
bojan <gagaleskibojan@gmail.com>
2021-01-06 23:32:41 +0100

    firstCommit

---
+++ script.js
@@ -0,0 +1,11 @@
+// const title = document.getElementById("project-title");
+// const img = document.getElementById("project-image")
+// function hoverOn(){
+//     img.classList.toggle("project-image-show")
+// }
+// function hoverOff(){
+//     img.classList.remove("project-image-show")
+// }
+// title.addEventListener('mouseover', hoverOn)
+// // title.addEventListener('mouseout', hoverOff)
+// img.addEventListener('mouseout', hoverOff)
